aa1505a8c3fb90fdec9851cda99a2843729ceccc
Nolawee Mengist <nome9478@colorado.edu>
2017-05-29 20:33:20 -0600

    transaction graph

--- src/Section.js
+++ src/Section.js
@@ -4,17 +4,20 @@ import Label from './components/Label';
 import Mission from './sections/Mission';
 import Opportunity from './sections/Opportunity';
 import Value from './sections/Value';
+import Traction from './sections/Traction';
 
 
 class Section extends Component {
   render() {
     var section;
-    if(this.props.section == 'Mission') {
+    if (this.props.section == 'Mission') {
       section = <Mission />;
-    } else if(this.props.section == 'Opportunity') {
+    } else if (this.props.section == 'Opportunity') {
       section = <Opportunity />;
-    } else if(this.props.section == 'Value') {
+    } else if (this.props.section == 'Value') {
       section = <Value />
+    }else if (this.props.section == 'Traction') {
+      section = <Traction />
     }
     return (
     <div className='section'>
--- src/sections/Traction.js
+++ src/sections/Traction.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
+import data from '../content/traction';
 
 class Traction extends Component {
   render() {
     return (
-    <div>
+    <div className="container">
+        <div className='header'>
+          <h2>The Journey Until Now...Boostrapped MoM Platform Growth</h2>
+        </div>
+        <div className='traction-image-container'>
+          <img className='traction-image' src={require('../assets/img/05-Market-Validation-Growth.png')} />
+        </div>
+          <div className='text-block-grid'>
+            <div className='text-block-row'>
+              {data.map((block,i) => (
+                <div className='text-block'>
+                  <h6>{block.title}</h6>
+                  <div className='text-block-description'>
+                    <div className='roles'>
+                      {block.list.map((item,i) => (
+                        <div className='role'>
+                          <p className='role-title'>{item}</p>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ))}
+              </div>
+            </div>
     </div>
   );
   }

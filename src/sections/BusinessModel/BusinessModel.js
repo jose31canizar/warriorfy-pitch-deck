a9c3579c95b8e56a1d1ac36c28fc0672b17c8deb
Nolawee Mengist <nome9478@colorado.edu>
2017-06-05 20:51:15 -0600

    BusinessModel

--- src/sections/BusinessModel/BusinessModel.js
+++ src/sections/BusinessModel/BusinessModel.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class BusinessModel extends Component {
   render() {
     return (
-    <div>
+    <div className="BusinessModel">
+      <div className='container'>
+        <div className='header'>
+        </div>
+        <div className='subtitle-text'>
+          <h1>NEED TO PUT IN A 3-TIERED IMAGE OF OUR SUBSCRIPTIONS (FREE, PRO, PERSONAL COACH)</h1>
+          <h3>1.) Freemium model with 5% conversion from free to paid plan</h3>
+          <h3>2.) At 1M users our projected MRR is $1.3M </h3>
+        </div>
+      </div>
     </div>
   );
   }

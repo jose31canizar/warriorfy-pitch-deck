c4437f2840e1916b078f80e87b3a6366c057231b
Nolawee Mengist <nome9478@colorado.edu>
2017-06-08 21:38:47 -0600

    a whole bunch of styling shit

--- src/sections/Investment/Investment.js
+++ src/sections/Investment/Investment.js
@@ -11,70 +11,53 @@ class Investment extends Component {
                         <h1>Seeking investors who believe healthcare needs more innovation.</h1>
                     </div>
                     <div className='text-block-grid-investment'>
-                        <div className='text-block-milestone'>
-                            <div className='header-text-milestone'>
+                        <div className='milestones'>
+                          <div className='milestone'>
+                            <div className='milestone-header'>
                                 <h6>Pre-Seed Round</h6>
                                 <div className='milestone-date'>
                                     <p>Close Date: July 11, 2017</p>
                                 </div>
                             </div>
-                            <div className='milestone'>
-                                <div className='milestone-title'>
-                                    <p>Launched Rise Up tracking app on Android</p>
-                                </div>
+                            <div className='goal'>
+                              <p>Launched Rise Up tracking app on Android</p>
                             </div>
-                            <div className='milestone'>
-                                <div className='milestone-title'>
-                                    <p>Forbes featured app as a technology innovating mental health</p>
-                                </div>
+                            <div className='goal'>
+                              <p>Forbes featured app as a technology innovating mental health</p>
                             </div>
-                            <div className='header-text-milestone'>
+                          </div>
+                          <div className='milestone'>
+                            <div className='milestone-header'>
                                 <h6>Seed Round</h6>
                                 <div className='milestone-date'>
                                     <p>Anticipated Close Date: October 11, 2017</p>
                                 </div>
-
                             </div>
-                            <div className='milestone'>
-                                <div className='milestone-title'>
-                                    <p>Doubled annual revenue to $86K through subscriptions to our treatment directory</p>
-                                </div>
+                            <div className='goal'>
+                              <p>Doubled annual revenue to $86K through treatment directory subscriptions</p>
                             </div>
-                            <div className='milestone'>
-                                <div className='milestone-title'>
-                                    <p>Sold $5k in subscriptions to Warriorfy pre-launch</p>
-                                </div>
+                            <div className='goal'>
+                              <p>Sold $5k in subscriptions pre-launch</p>
                             </div>
-                            <div className='milestone'>
-                                <div className='milestone-title'>
-                                    <p>Launch web-based version of Warriorfy</p>
-                                </div>
+                            <div className='goal'>
+                              <p>Launch a web-based version</p>
                             </div>
-                            <div className='milestone'>
-                                <div className='milestone-title'>
-                                    <p>Launch mobile version of Warriorfy</p>
-                                </div>
+                            <div className='goal'>
+                              <p>Launch a mobile version</p>
                             </div>
-                            <div className='milestone'>
-                                <div className='milestone-title'>
-                                    <p>25,000 users ($33K MRR)</p>
-                                </div>
+                            <div className='goal'>
+                              <p>25,000 users ($33K MRR)</p>
                             </div>
-                            <div className='milestone'>
-                                <div className='milestone-title'>
-                                    <p>Add 1:1 coaching as a service</p>
-                                </div>
+                            <div className='goal'>
+                              <p>Add 1:1 coaching as a service</p>
                             </div>
-                            <div className='milestone'>
-                                <div className='milestone-title'>
-                                    <p>100,000 users ($130K MRR)</p>
-                                </div>
+                            <div className='goal'>
+                              <p>100,000 users ($130K MRR)</p>
                             </div>
-                            <div className='milestone'>
-                                <div className='milestone-title'>
-                                    <p>1,000,000 users ($1.3M MRR)</p>
-                                </div>
+                            <div className='goal'>
+                              <p>1,000,000 users ($1.3M MRR)</p>
                             </div>
+                          </div>
                         </div>
                         <div className='text-block-container'>
                             <div className='header-text-money'>
